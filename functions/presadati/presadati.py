d7c74b1f00a4d66b5442bff97440a9ad764ff20f
z.matteo29@gmail.com <z.matteo29@gmail.com>
2019-10-20 17:18:52 +0200

    Aggiunta scrittura su file

--- functions/presadati/presadati.py
+++ functions/presadati/presadati.py
@@ -6,15 +6,22 @@ import csv
 
 matplotlib.rcParams.update({'font.size': 22}) # Cambio dimensione font globale dei plot
 
-nomefile = "lab1.csv" # Nome del file su cui salvare i dati (ed eventualmente caricare quelli esistenti)
+file_lettura = "lab1.csv" # Nome del file da cui caricare quelli esistenti.
+file_scrittura = "misure1.csv" # Nome del file dove salvare tutti i dati
 
 # Array di storage dove tenere tutti i dati (che poi andranno anche salvati su un csv)
 xdata = []
 ydata = []
 sigmadata = []
 
+# cd nella directory di questo file (non sempre ci troviamo qui in automatico)
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 # Funzione di plotting, la faccio una volta per tutte
 def plotxy(x , y):
+    print(type(y[-1]))
     plt.plot(x, y, 'go--', linewidth=2, markersize=12, color=[0.01, 0.01, 0.01], alpha=0.5, markerfacecolor=[0, 0, 1])
     plt.grid()
     plt.xlabel("x")
@@ -22,36 +29,42 @@ def plotxy(x , y):
     plt.title("X-Y Plot")
     plt.show()
 
-# cd nella directory di questo file (non sempre ci troviamo qui in automatico)
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+# Funzione di lettura dati già presenti
+def leggiFile(nomefile):
+    myfile = os.path.join(file_lettura)
+    if os.path.isfile(myfile):
+        with open(file_lettura, 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            # Per ogni riga del file, aggiungo la tripletta all'array di storage
+            for r in reader:
+                row = [float(r[0]), float(r[1]), float(r[2])]
+                xdata.append(row[0])
+                ydata.append(row[1])
+                sigmadata.append(row[2])
+                aggiornaFile(file_scrittura, row) # Funzione che scrive in modalità "append" sul file di scrittura
 
-#-------- Cerco se esiste già un file con questo nome ed eventualmente carico quei dati ----------#
-myfile = os.path.join(nomefile)
-if os.path.isfile(myfile):
-    with open(nomefile, 'r') as csvFile:
-        reader = csv.reader(csvFile)
-        # Per ogni riga del file, aggiungo la tripletta all'array di storage
-        for row in reader:
-            xdata.append(row[0])
-            ydata.append(row[1])
-            sigmadata.append(row[2])
-    plotxy(xdata, ydata)
-#-------------------------------------------------------------------------------------------------#
+# Funzione di scrittura su file
+def aggiornaFile(nomefile, xysigma):
+    with open(nomefile, 'a') as filew:
+        writer = csv.writer(filew, delimiter=',')
+        writer.writerow(xysigma)
 
-while(True): # Arresto il ciclo quando vengono inseriti dei caratteri
+leggiFile(file_lettura)
+plotxy(xdata, ydata)
+
+while(True):
     print("Inserire una tripla (x, y, sigma) nel formato 'dato1 dato2 dato3' per aggiungerla al grafico.")
     nuovo_dato = input()
 
     if(len(nuovo_dato.split()) == 3):
         x, y, sigma = nuovo_dato.split() # divido i valori in tre variabili diverse
         # Aggiungo i nuovi dati agli array di storage
-        xdata.append(x)
-        ydata.append(y)
-        sigmadata.append(sigma)
+        xdata.append(float(x))
+        ydata.append(float(y))
+        sigmadata.append(float(sigma))
+        # Scrivo su file
+        aggiornaFile(file_scrittura, [x, y, sigma])
         # Grafico
         plotxy(xdata, ydata)
     else:
         print("Input non riconosciuto")
-        break
