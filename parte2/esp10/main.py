84466ce96ba3b9a90ad0ff1b90a4b21edbe68090
Matteo294 <zmatteo.29@gmail.com>
2020-06-23 10:58:47 +0200

    Ciclo su distanze

--- parte2/esp10/main.py
+++ parte2/esp10/main.py
@@ -3,8 +3,11 @@
 from libphysics import *
 from estrazione_segnale_funzione import *
 import numpy as np
+import sys
 
-input_file = "data_Faraday.csv"
+plot_flag = 0
+if len(sys.argv) > 1:
+    plot_flag = int(sys.argv[1])
 
 # Misure e costanti
 Gdiff = 0 # da calcolare
@@ -16,53 +19,69 @@ dR_lim = 0
 n_samples = 0
 Mrs = 0
 
+distanze = []
 frequenze = [10e3, 200e3] # fmax e fmin
 
-Z_eff = []
-dZ_eff = []
+# Array induttanza mutua a diverse distanze
+Mrs = [] 
+dMrs = []
 
-for i, f in enumerate(frequenze):
+for d in distanze:
 
-    A_in, B_in, A_out, B_out = [[] for n in range(4)]
+    base_input_file = "Faraday" + int(d)
 
-    for j in range(n_samples):
-        segnale = estrazione_segnale(input_file, f, showplots=False)
-        A_in.append(segnale["A_in"])
-        B_in.append(segnale["B_in"])
-        A_out.append(segnale["A_out"])
-        B_out.append(segnale["B_out"])
-    
-    # Valori medi delle misure ripetute
-    A_in_media = np.average(A_in)
-    A_in_std = np.std(A_in, ddof=1)
+    Z_eff = []
+    dZ_eff = []
 
-    B_in_media = np.average(B_in)
-    B_in_std = np.std(B_in, ddof=1)
+    for i, f in enumerate(frequenze):
 
-    A_out_media = np.average(A_out)
-    A_out_std = np.std(A_out, ddof=1)
+        A_in, B_in, A_out, B_out = [[] for n in range(4)]
 
-    B_out_media = np.average(B_out)
-    B_out_std = np.std(B_out, ddof=1)
+        for j in range(n_samples):
+            filename = base_input_file + str(i+1) + '.csv'
+            segnale = estrazione_segnale(filename, f, showplots=False)
+            A_in.append(segnale["A_in"])
+            B_in.append(segnale["B_in"])
+            A_out.append(segnale["A_out"])
+            B_out.append(segnale["B_out"])
+        
+        # Valori medi delle misure ripetute
+        A_in_media = np.average(A_in)
+        A_in_std = np.std(A_in, ddof=1)
 
-    # Calcolo ampiezza complessa del segnale in ingresso
-    C_in = A_in_media - 1j*B_in_media
-    dC_in = np.sqrt(A_in_std**2 + B_in_std**2)
+        B_in_media = np.average(B_in)
+        B_in_std = np.std(B_in, ddof=1)
 
-    # Calcolo ampiezza complessa segnale in uscita
-    C_out = A_out_media - 1j*B_out_media
-    dC_out = np.sqrt(A_out_std**2 + B_out_std**2)
+        A_out_media = np.average(A_out)
+        A_out_std = np.std(A_out, ddof=1)
 
-    # Impedenza efficace (vediamo lo spazio tra le due bobine come un induttore di induttanza Mrs)
-    i_s = C_in / R_lim
-    di_s = i_s * np.sqrt((dC_in/C_in)**2 + (dR_lim/R_lim)**2) 
-    Z_eff.append(np.imag(C_out / i_s)) # (Z dovrebbe essere puramente immaginaria, c'è solo la mutua induzione)
-    _dZ_eff = Z_eff * np.sqrt((dC_out/C_out)**2 + (di_s/i_s)**2)
-    dZ_eff.append(_dZ_eff * np.imag(Z_eff)/np.absolute(Z_eff))
+        B_out_media = np.average(B_out)
+        B_out_std = np.std(B_out, ddof=1)
 
-Ze = linreg(2*pi*frequenze, Z_eff, dZ_eff)
-print("Risultati: \t Z0 =", Ze['b'], "\u00B1", Ze['db'], " \t Zeff =", Ze['m'], "\u00B1", Ze['dm'])
+        # Calcolo ampiezza complessa del segnale in ingresso
+        C_in = A_in_media - 1j*B_in_media
+        dC_in = np.sqrt(A_in_std**2 + B_in_std**2)
 
-fig = bodeplot(frequenze, H=C_out)
+        # Calcolo ampiezza complessa segnale in uscita
+        C_out = A_out_media - 1j*B_out_media
+        dC_out = np.sqrt(A_out_std**2 + B_out_std**2)
 
+        # Impedenza efficace (vediamo lo spazio tra le due bobine come un induttore di induttanza Mrs)
+        i_s = C_in / R_lim
+        di_s = i_s * np.sqrt((dC_in/C_in)**2 + (dR_lim/R_lim)**2) 
+        Z_eff.append(np.imag(C_out / i_s)) # (Z dovrebbe essere puramente immaginaria, c'è solo la mutua induzione)
+        _dZ_eff = Z_eff * np.sqrt((dC_out/C_out)**2 + (di_s/i_s)**2)
+        dZ_eff.append(_dZ_eff * np.imag(Z_eff)/np.absolute(Z_eff))
 
+    Ze = linreg(2*pi*frequenze, Z_eff, dZ_eff)
+    print("Risultati d =", d, ": \t Z0 =", Ze['b'], "\u00B1", Ze['db'], " \t Zeff =", Ze['m'], "\u00B1", Ze['dm'])
+
+    if plot_flag == 1:
+        fig = bodeplot(frequenze, H=C_out)
+        fig.show()
+
+    Mrs.append(Ze)
+    dMrs.append(dZe)
+
+plt.errorbar(distanze, Mrs, yerr=dMrs, '.')
+plt.show()
