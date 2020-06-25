8b728464713e1e525dee7e40458ff741bb3a88e8
Matteo294 <zmatteo.29@gmail.com>
2020-06-25 09:30:22 +0200

    Gdelta su file

--- parte2/esp9/Con_sorgente/differenziale_sorgente.py
+++ parte2/esp9/Con_sorgente/differenziale_sorgente.py
@@ -7,11 +7,13 @@ Re = 120
 
 # prendo Gcm dall'output dell'altro script
 file_gcm = "output_common_mode_sorgente.csv"
+file_gdelta = "output_differential_mode_sorgente.csv"
 [Gcm, Gcm_fase, dGcm, dGcm_fase] = readCSV(file_gcm)
 
 # preparo gli array per il guadagno (complesso, contiene dentro fase e ampiezza non separate)
 Gdiff = []
 Gdiff_fase = []
+Gdiff_complesso = []
 
 for i, freq in enumerate(freqs):    # ciclo sulle frequenze
     input_file = "Data_diff/" + str(i+1) + ".csv"
@@ -25,14 +27,19 @@ for i, freq in enumerate(freqs):    # ciclo sulle frequenze
     C_out = A_out - 1j*B_out
     H = C_out/C_in
     # Gdiff = Vout/Vin - Gcm/2, ma sono numeri complessi
-    Gdiff_complesso = H - Gcm[i]*np.exp(1j*Gcm_fase[i])
-    Gdiff.append(float(abs(Gdiff_complesso)))
-    Gdiff_fase.append(float(np.angle(Gdiff_complesso)))
+    Gdiff_complesso.append(H - Gcm[i]*np.exp(1j*Gcm_fase[i]))
+    Gdiff.append(float(abs(Gdiff_complesso[i])))
+    Gdiff_fase.append(float(np.angle(Gdiff_complesso[i])))
+
+outfile = open(file_gdelta, 'w+')
+for f, G in zip(freqs, Gdiff_complesso):
+    outfile.write(str(f) + "," + str(G[0]) + '\n')
 
 Gdiff = numpify(Gdiff)
 Gdiff_fase = numpify(Gdiff_fase)
 Gcm = numpify(Gcm[:-1])
 Gcm_fase = numpify(Gcm_fase[:-1])
+
 freqs = numpify(freqs)
 
 #Cmrr = abs(Gdiff/Gcm)
